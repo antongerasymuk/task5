12234ec8167c47b8dcf277e7cc1b25ae86f54f5b
Anton <bron_off1@ukr.net>
2016-11-20 10:29:30 +0200

    add user index controller

---
+++ frontend/controllers/IndexController.php
@@ -0,0 +1,52 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+
+/**
+ * Site controller
+ */
+class IndexController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        echo "!!!!MY TEST INDEX ACTION!!!!";
+        exit;
+    }
+
+}
